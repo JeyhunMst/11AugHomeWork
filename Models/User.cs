d97435f398d611fed71ae1f0d7ba385b7efae3ba
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-11 12:25:06 +0400

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11AugHomeWork.Models
+{
+    class User
+    {
+        private string UserName;
+
+        public string _userName
+        {
+            get
+            {
+                
+               return UserName;
+                
+                
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25 )
+                {
+                    UserName = value;
+                }
+            }
+
+
+        }
+        private string Password;
+
+
+
+        public string _password
+        {
+            get
+            {
+
+                return Password;
+
+
+
+            }
+            set
+            {
+                if (HasUpper(value) == true && HasLower(value) == true && HasDigit(value) == true && value.Length >= 8 && value.Length <= 25 )
+                {
+                    Password = value;
+                }
+            }
+        }
+
+        
+
+        public void ShowInfo()
+        {
+            if (UserName != null && Password != null)
+            {
+                Console.WriteLine($"Username:{UserName} Password:{Password}");
+            }
+            else
+                Console.WriteLine("Username or Password is wrong");
+
+        }
+        public bool HasDigit(string hasDigit)
+        {
+            bool checkNumber = false;
+            foreach (char item in hasDigit)
+            {
+                checkNumber = Char.IsDigit(item);
+                if (checkNumber == true)
+                {
+                    break;
+                }
+            }
+            return checkNumber;
+        }
+        public bool HasLower(string hasLower)
+        {
+            bool checkLower = false;
+            foreach (char item in hasLower)
+            {
+                checkLower = Char.IsLower(item);
+                if (checkLower == true)
+                {
+                    break;
+                }
+            }
+            return checkLower;
+        }
+        public bool HasUpper(string hasUpper)
+        {
+            bool checkUpper = false;
+            foreach (char item in hasUpper)
+            {
+                checkUpper = Char.IsUpper(item);
+                if (checkUpper == true)
+                {
+                    break;
+                }
+            }
+            return checkUpper;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }       
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using _11AugHomeWork.Models;
+using System;
+
+namespace _11AugHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User Developer = new User();
+            Console.WriteLine("UserName:");
+            Developer._userName = Console.ReadLine();
+            Console.WriteLine("Password:");
+            Developer._password = Console.ReadLine();
+            
+            
+                
+            
+
+            Developer.ShowInfo();
+        }
+    }
+}
